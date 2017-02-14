a1e6e200a2b38d664c2ae894d61293a12865c3fc
marineco <marinecolonge@gmail.com>
2017-02-14 22:30:26 +0100

    a little of css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,26 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Sweets shop</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.7/semantic.min.css">
 </head>
 <body>
-	<h1>Sweets shop</h1>
+	<div class="ui center aligned container">
+		<h1 class="ui pink block header"><i class="shop icon"></i>Sweets shop</h1>
+	</div>
 	<div>
-		<table>
-			<tr>
-				<td>Name</td>
-				<td>Quantity</td>
-			</tr>
-			<tr>
-				<td id="name"></td>
-				<td id="qty"></td>
-			</tr>
+		<table class="ui unstackable table">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Quantity</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th id="name"></th>
+					<th id="qty"></th>
+				</tr>
+			</tbody>
 		</table>
 
 	</div>
